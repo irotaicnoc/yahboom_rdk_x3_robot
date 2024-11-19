b27916a8f4d2c59458583f74fbfb994c76199704
Marco Conciatori <conciatori.marco91@gmail.com>
2024-11-19 16:32:14 +0100

    added config file and command line arguments

---
+++ sunriseRobot/app_SunriseRobot/args.py
@@ -0,0 +1,39 @@
+import yaml
+import copy
+import argparse
+
+
+def import_args(yaml_path: str, **kwargs) -> dict:
+    data_dict = None
+    with open(yaml_path) as f:
+        data_dict = yaml.safe_load(f)
+
+    # command line arguments have priority over yaml arguments
+    data_dict = from_command_line(default_data_dict=data_dict)
+    # function arguments have priority over yaml AND command line arguments
+    data_dict = from_function_arguments(default_data_dict=data_dict, **kwargs)
+
+    return data_dict
+
+
+def from_command_line(default_data_dict: dict) -> dict:
+    parser = argparse.ArgumentParser()
+    for key in default_data_dict:
+        value = default_data_dict[key]
+        parser.add_argument(f'--{key}', dest=key, type=type(value))
+
+    updated_data_dict = copy.deepcopy(default_data_dict)
+    args = parser.parse_args()
+    args_dict = vars(args)
+    for key in args_dict:
+        updated_data_dict[key] = args_dict[key]
+
+    return updated_data_dict
+
+
+def from_function_arguments(default_data_dict: dict, **kwargs) -> dict:
+    updated_data_dict = copy.deepcopy(default_data_dict)
+    for key in kwargs:
+        updated_data_dict[key] = kwargs[key]
+
+    return updated_data_dict
