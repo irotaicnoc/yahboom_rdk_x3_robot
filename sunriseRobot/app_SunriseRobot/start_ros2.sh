cdb7d398f17c9a1afadf19573b4b51dfb0f8e9ad
Marco Conciatori <irotaicnoc@gmail.com>
2024-11-26 14:18:55 +0100

    added .sh file for ros2 launch (partial)

---
+++ sunriseRobot/app_SunriseRobot/start_ros2.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+###############################################################################
+# ubuntu 20.04 
+# start ros2
+# bash /root/sunriseRobot/app_SunriseRobot/start_ros2.sh
+# start app program
+###############################################################################
+
+
+# ubuntu 20.04
+gnome-terminal -- bash -c "python3 /root/sunriseRobot/app_SunriseRobot/start_ros2.py;exec bash"
+
+
+wait
+exit 0
