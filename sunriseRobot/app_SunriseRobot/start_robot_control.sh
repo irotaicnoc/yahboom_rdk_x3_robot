d981d78cc573ba365e6b2575205284dd5e53c60c
Marco Conciatori <irotaicnoc@gmail.com>
2024-11-26 14:17:48 +0100

    changed .sh file for automatic startup

--- sunriseRobot/app_SunriseRobot/start_robot_control.sh
+++ sunriseRobot/app_SunriseRobot/start_robot_control.sh
@@ -3,13 +3,13 @@
 ###############################################################################
 # ubuntu 20.04 
 # add Additional startup programs
-# start_joystick
-# bash /root/sunriseRobot/app_SunriseRobot/start_joystick.sh
+# main thread
+# bash /root/sunriseRobot/app_SunriseRobot/main_thread.sh
 # start app program
 ###############################################################################
 
 
-sleep 8
+sleep 2
 
 # ubuntu 20.04
 gnome-terminal -- bash -c "python3 /root/sunriseRobot/app_SunriseRobot/main_thread.py;exec bash"
