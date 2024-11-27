097dec1c2d0871731fffc818c206c054904758e9
Marco Conciatori <irotaicnoc@gmail.com>
2024-11-27 22:58:18 +0100

    added sleep

--- sunriseRobot/app_SunriseRobot/start_ros2.sh
+++ sunriseRobot/app_SunriseRobot/start_ros2.sh
@@ -7,6 +7,8 @@
 # start app program
 ###############################################################################
 
+sleep 1
+
 # ubuntu 20.04
 gnome-terminal -- bash -c "cd /root/marco_ros2_ws/;ros2 launch ros_tcp_endpoint endpoint_launch.py;exec bash"
 
