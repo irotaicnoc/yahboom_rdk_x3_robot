dfb0aea5f7b6ec514e4ecbf3127932d02500c936
Marco Conciatori <irotaicnoc@gmail.com>
2024-11-27 19:27:05 +0100

    test: added simple launcher for ros2 files

---
+++ src/camera_pub/launch/camera_controller_launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Camera publisher node
+    camera_publisher_node = Node(
+        package='camera_pub',
+        executable='camera_publisher_node',
+    )
+
+    # Controller subscriber node
+    controller_subscriber_node = Node(
+        package='controller_sub',
+        executable='controller_subscriber_node',
+    )
+
+    # Create list of nodes to launch
+    node_list = [
+        camera_publisher_node,
+        controller_subscriber_node,
+    ]
+
+    return LaunchDescription(node_list)
