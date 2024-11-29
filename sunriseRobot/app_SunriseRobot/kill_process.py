b86c5f5cc64c0a10660735137f95607660d4deb8
Marco Conciatori <irotaicnoc@gmail.com>
2024-11-29 16:24:11 +0100

    improved kill_process

--- sunriseRobot/app_SunriseRobot/kill_process.py
+++ sunriseRobot/app_SunriseRobot/kill_process.py
@@ -2,15 +2,19 @@ import os
 import psutil
 
 
-def kill_process_(program_name: str, debug: bool = False):
-    process_list = psutil.process_iter()
-    if debug:
-        print(f'Killing for {program_name}...')
-    for process in process_list:
-        if program_name in process.name():
-            if debug:
-                print(f'\t\t{process.name()} is running')
-            os.kill(process.pid, 9)
-            if debug:
-                print(f'\t\t{process.name()} killed')
-            os.system('sleep 0.1')
+def kill_process_(program_name: str, verbose: bool = False):
+    target_found = True
+    while target_found:
+        target_found = False
+        process_list = psutil.process_iter()
+        if verbose:
+            print(f'Killing for {program_name}...')
+        for process in process_list:
+            if program_name in process.name():
+                target_found = True
+                if verbose:
+                    print(f'\t\t{process.name()} is running')
+                os.kill(process.pid, 9)
+                if verbose:
+                    print(f'\t\t{process.name()} killed')
+                os.system('sleep 0.1')
