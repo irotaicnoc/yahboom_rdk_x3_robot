bdc525f7a1e4dfc777b8d9faadf0e8642847c041
Marco Conciatori <irotaicnoc@gmail.com>
2024-12-05 00:21:49 +0100

    test pin with led

--- sunriseRobot/app_SunriseRobot/gpio_pin/test_single_pin.py
+++ sunriseRobot/app_SunriseRobot/gpio_pin/test_single_pin.py
@@ -2,15 +2,17 @@ import time
 
 import Hobot.GPIO as GPIO
 
-# Define the GPIO channel used as 38
-output_pin = 38  # BOARD code 38
+# import global_constants as gc
+from .. import global_constants as gc
 
 
 def main():
     # Set the pin numbering mode to BOARD
     GPIO.setmode(GPIO.BOARD)
     # Set the pin as output and initialize it to high level
-    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(gc.VIOLET_CABLE, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(gc.BLUE_CABLE, GPIO.OUT, initial=GPIO.LOW)
+
     # Record the current pin state
     curr_value = GPIO.HIGH
     print("Starting demo now! Press CTRL+C to exit")
@@ -18,7 +20,7 @@ def main():
         # Loop to control the LED light on and off every 1 second
         while True:
             time.sleep(1)
-            GPIO.output(output_pin, curr_value)
+            GPIO.output(gc.VIOLET_CABLE, curr_value)
             curr_value ^= GPIO.HIGH
     finally:
         GPIO.cleanup()
