1ba35b8e486e11eb4efb68601e8ef7550061c166
Marco Conciatori <irotaicnoc@gmail.com>
2024-12-05 00:44:57 +0100

    test RG led

--- sunriseRobot/app_SunriseRobot/test_single_pin.py
+++ sunriseRobot/app_SunriseRobot/test_single_pin.py
@@ -9,20 +9,26 @@ def main():
     # Set the pin numbering mode to BOARD
     GPIO.setmode(GPIO.BOARD)
     # Set the pin as output and initialize it to high level
-    GPIO.setup(gc.VIOLET_CABLE, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(gc.VIOLET_CABLE, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(gc.BLUE_CABLE, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(gc.GREEN_CABLE, GPIO.OUT, initial=GPIO.LOW)
 
     # Record the current pin state
-    curr_value = GPIO.HIGH
+    red_light = GPIO.HIGH
+    green_light = GPIO.LOW
     print("Starting demo now! Press CTRL+C to exit")
-    try:
-        # Loop to control the LED light on and off every 1 second
-        while True:
-            time.sleep(1)
-            GPIO.output(gc.VIOLET_CABLE, curr_value)
-            curr_value ^= GPIO.HIGH
-    finally:
-        GPIO.cleanup()
+    # Loop to control the LED light on and off every 1 second
+    counter = 0
+    while True:
+        time.sleep(1)
+        GPIO.output(gc.VIOLET_CABLE, red_light)
+        GPIO.output(gc.GREEN_CABLE, green_light)
+        if counter % 2 == 0:
+            red_light ^= GPIO.HIGH
+        if counter % 3 == 0:
+            green_light ^= GPIO.HIGH
+
+        counter += 1
 
 
 if __name__ == '__main__':
