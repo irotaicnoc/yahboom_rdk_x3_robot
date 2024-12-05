000ebeddae6cc011ad1c0881c3800a5284316281
Marco Conciatori <irotaicnoc@gmail.com>
2024-12-05 13:08:58 +0100

    all start script moved in the same folder

--- sunriseRobot/app_SunriseRobot/utils.py
+++ sunriseRobot/app_SunriseRobot/utils.py
@@ -113,7 +113,7 @@ def deactivate_hotspot(verbose: int = 0):
 def activate_ros2(verbose: int = 0):
     if verbose >= 1:
         print('Starting ROS2...', end='')
-    # os.system(f'{gc.APP_FOLDER_PATH}start_ros2.sh')
+    # os.system(f'{gc.SCRIPT_FOLDER_PATH}start_ros2.sh')
     os.system('gnome-terminal -- bash -c "source /opt/ros/foxy/setup.bash;cd /root/marco_ros2_ws/;'
               'source install/local_setup.bash;ros2 launch ros_tcp_endpoint endpoint_launch.py;exec bash"')
     # os.system('wait')
