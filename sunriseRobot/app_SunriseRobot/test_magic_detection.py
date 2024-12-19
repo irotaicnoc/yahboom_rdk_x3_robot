c141de6c1eb8f4ae0f28f50b6fe419705b6d2492
Marco Conciatori <irotaicnoc@gmail.com>
2024-12-19 22:12:18 +0100

    updated test script

--- sunriseRobot/app_SunriseRobot/test_magic_detection.py
+++ sunriseRobot/app_SunriseRobot/test_magic_detection.py
@@ -13,12 +13,12 @@ def test_magic_detector() -> None:
     # trained_model_path = f'{gc.MAGIC_MODEL_FOLDER_PATH}magic_detector_yolo11l_640_480_edgetpu.tflite'
     trained_model = YOLO(model=trained_model_path, task='detect')
 
-    test_image_path_generator = Path(f'{gc.APP_FOLDER_PATH}test_images/').glob('*.jpg')
+    test_image_path_generator = Path(f'{gc.APP_FOLDER_PATH}test_images/').glob('*.*')
     for test_image_path in test_image_path_generator:
         str_path = str(test_image_path)
         print(f'image path: {str_path}')
         test_image = cv2.imread(filename=str_path)
-        utils.display_image(image=test_image, window_name='Test image')
+        # utils.display_image(image=test_image, window_name='Test image')
         # cv2.resize(src=test_image, dsize=(640, 640), dst=test_image)
         print(f'Test image shape: {test_image.shape}')
         # exchange the height and width
