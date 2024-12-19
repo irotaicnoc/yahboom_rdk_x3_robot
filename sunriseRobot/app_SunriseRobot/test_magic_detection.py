442b2e9e4993c279785eb6241a29465b91cd3cff
Marco Conciatori <irotaicnoc@gmail.com>
2024-12-19 19:18:40 +0100

    updated test script

--- sunriseRobot/app_SunriseRobot/test_magic_detection.py
+++ sunriseRobot/app_SunriseRobot/test_magic_detection.py
@@ -2,19 +2,20 @@ import cv2
 from pathlib import Path
 from ultralytics import YOLO
 
-import args
 import utils
 import global_constants as gc
 
 
-def test(**kwargs) -> None:
-    # model to test
+def test_magic_detector() -> None:
+    # magic detector model to test
     trained_model_path = f'{gc.MAGIC_MODEL_FOLDER_PATH}magic_detector_yolo11x_640_480_edgetpu.tflite'
-    trained_model = YOLO(model=trained_model_path)
+    trained_model = YOLO(model=trained_model_path, task='detect')
 
     test_image_path_generator = Path(f'{gc.APP_FOLDER_PATH}test_images/').glob('*.jpg')
     for test_image_path in test_image_path_generator:
-        test_image = cv2.imread(test_image_path)
+        str_path = str(test_image_path)
+        print(f'image path: {str_path}')
+        test_image = cv2.imread(filename=str_path)
         test_height, test_width = test_image.shape[:2]
         print(f'Test image (height, width): ({test_height}, {test_width})')
 
@@ -24,4 +25,4 @@ def test(**kwargs) -> None:
 
 
 if __name__ == '__main__':
-    test()
+    test_magic_detector()
