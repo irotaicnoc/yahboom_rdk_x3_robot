7d00cc89c8d65b0dcd4a2a6f5ef2a695cb425e78
Marco <irotaicnoc@gmail.com>
2025-04-17 16:55:45 +0200

    fix: not printed output to console

--- sunriseRobot/app_SunriseRobot/lidar_experiment.py
+++ sunriseRobot/app_SunriseRobot/lidar_experiment.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import time
 
@@ -15,19 +16,23 @@ def obstacle_sensor():
         verbose=3,
     )
     print('LidarListener initialized')
+    ascii_circle = [[' ' for _ in range(20)] for _ in range(20)]
+    for i in range(20):
+        for j in range(20):
+            if (i - 10) ** 2 + (j - 10) ** 2 <= 100:
+                ascii_circle[i][j] = 'O'
 
     try:
+        counter = 0
         while True:
+            print(f'Counter: {counter}')
             # os.system('clear')
+            canvas = copy.deepcopy(ascii_circle)
             lidar_data = lidar_listener_node.read_lidar_data()
             if lidar_data is not None:
                 _, obstacles_by_sector, average_distance_by_sector = lidar_data
                 # print an ascii art circle and add detected obstacles as 'X' using their direction and distance
-                ascii_circle = [[' ' for _ in range(20)] for _ in range(20)]
-                for i in range(20):
-                    for j in range(20):
-                        if (i - 10) ** 2 + (j - 10) ** 2 <= 100:
-                            ascii_circle[i][j] = 'O'
+
                 for sector_num in range(len(obstacles_by_sector)):
                     if obstacles_by_sector[sector_num]:
                         angle = sector_num * lidar_listener_node.sector_angle
@@ -35,10 +40,17 @@ def obstacle_sensor():
                         x = int(10 + distance * 10 * (angle / 180))
                         y = int(10 - distance * 10 * (angle / 180))
                         if 0 <= x < 20 and 0 <= y < 20:
-                            ascii_circle[y][x] = 'X'
+                            canvas[y][x] = 'X'
 
-            time.sleep(0.5)
+            print(canvas)
+
+            # wait for key press to continue
+            input()
+
+            # time.sleep(1)
             os.system('clear')
+            counter += 1
+
     except KeyboardInterrupt:
         print('Experiment stopped by user.')
         lidar_listener_node.delete_listener()
