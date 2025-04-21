ce6e423ace607bd7ca3a0823a1720219dac4f6b5
Marco <irotaicnoc@gmail.com>
2025-04-21 23:26:00 +0200

    added lidar assisted braking to user control wheels mode (partial)

--- sunriseRobot/app_SunriseRobot/utils.py
+++ sunriseRobot/app_SunriseRobot/utils.py
@@ -240,6 +240,6 @@ def calculate_robot_direction(speed_x: float, speed_y: float, speed_z: float) ->
         angle_degrees = np.degrees(angle_radian)
         angle_degrees = (angle_degrees + 90) % 360
         # rotate by speed_z
-        angle_degrees += speed_z * 40
+        angle_degrees += speed_z * 200
 
         return angle_degrees
