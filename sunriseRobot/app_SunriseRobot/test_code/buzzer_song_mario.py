0addbdbd13da6eb95cc191fd4fe6cbfd83a2de3a
Marco <irotaicnoc@gmail.com>
2025-04-23 03:29:15 +0200

    added buzzer test tune

---
+++ sunriseRobot/app_SunriseRobot/test_code/buzzer_song_mario.py
@@ -0,0 +1,39 @@
+import time
+
+import args
+import global_constants as gc
+from robot_body import RobotBody
+
+
+kwargs = {}
+parameters = args.import_args(yaml_path=gc.CONFIG_FOLDER_PATH + 'main_thread.yaml', **kwargs)
+
+robot_body = RobotBody(com=parameters['com'], baud_rate=parameters['baud_rate'], verbose=parameters['verbose'])
+robot_body.create_receive_threading()
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(300)
+robot_body.set_beep(300)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(300)
+robot_body.set_beep(300)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(300)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(100)
+robot_body.set_beep(400)
