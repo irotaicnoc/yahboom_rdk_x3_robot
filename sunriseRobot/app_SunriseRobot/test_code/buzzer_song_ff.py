4499613008eafdf12184d3421c8bd733bbee396c
Marco <irotaicnoc@gmail.com>
2025-05-07 18:50:24 +0200

    removed imports

--- sunriseRobot/app_SunriseRobot/test_code/buzzer_song_ff.py
+++ sunriseRobot/app_SunriseRobot/test_code/buzzer_song_ff.py
@@ -1,14 +1,9 @@
 import time
 
-import args
-import global_constants as gc
 from robot_body import RobotBody
 
 
-kwargs = {}
-parameters = args.import_args(yaml_path=gc.CONFIG_FOLDER_PATH + 'main_thread.yaml', **kwargs)
-
-robot_body = RobotBody(com=parameters['com'], baud_rate=parameters['baud_rate'], verbose=parameters['verbose'])
+robot_body = RobotBody(verbose=2)
 robot_body.create_receive_threading()
 robot_body.set_beep(100)
 robot_body.set_beep(100)
--- sunriseRobot/app_SunriseRobot/test_code/buzzer_song_mario.py
+++ sunriseRobot/app_SunriseRobot/test_code/buzzer_song_mario.py
@@ -1,12 +1,7 @@
-from .. import args
 from robot_body import RobotBody
-from .. import global_constants as gc
 
 
-kwargs = {}
-parameters = args.import_args(yaml_path=gc.CONFIG_FOLDER_PATH + 'main_thread.yaml', **kwargs)
-
-robot_body = RobotBody(com=parameters['com'], baud_rate=parameters['baud_rate'], verbose=parameters['verbose'])
+robot_body = RobotBody(verbose=2)
 robot_body.create_receive_threading()
 robot_body.set_beep(100)
 robot_body.set_beep(100)
