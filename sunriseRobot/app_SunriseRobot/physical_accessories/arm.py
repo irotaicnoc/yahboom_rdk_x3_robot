b10466003dd093faa84292f13d14a5ac6bf30d0f
Marco <irotaicnoc@gmail.com>
2025-05-13 20:56:02 +0200

    fix: correct iteration number printed

--- sunriseRobot/app_SunriseRobot/physical_accessories/arm.py
+++ sunriseRobot/app_SunriseRobot/physical_accessories/arm.py
@@ -93,7 +93,7 @@ class Arm:
         counter = 0
         while -1 in angle_list:
             if counter > 1:
-                print(f'first two readings got an error, try n°: {counter}')
+                print(f'first two readings got an error, try n°: {counter + 1}')
                 print(f'current angles: {angle_list}')
             temp_angle_list = self.robot_body.get_arm_angle_list()
             for angle_id in range(len(temp_angle_list)):
