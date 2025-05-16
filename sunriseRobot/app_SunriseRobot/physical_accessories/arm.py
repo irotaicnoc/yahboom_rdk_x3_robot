5b159d642c5290d1934c6be776fe2263f543578e
Marco <irotaicnoc@gmail.com>
2025-05-16 14:22:16 +0200

    testing parameter arm_speed_proportion_ik

--- sunriseRobot/app_SunriseRobot/physical_accessories/arm.py
+++ sunriseRobot/app_SunriseRobot/physical_accessories/arm.py
@@ -1,4 +1,5 @@
 import copy
+import time
 import warnings
 import numpy as np
 
@@ -125,7 +126,9 @@ class Arm:
             # print(f'gripper_pos after:  {self.gripper_pos}')
             # the function returns 6 angle_list, but we don't need the first and last ones, they should be the
             # gripper rotation and opening. But the 2 excluded angles are the last 2 angles in the list
+            start_time = time.time()
             ikpy_angle_list = self.servo_chain.inverse_kinematics(target_position=self.gripper_pos)
+            print(f'IK computation time: {round(time.time() - start_time), 2} seconds')
             # print(f'ikpy_angle_list: {ikpy_angle_list}')
             # print(f'desired_angle_list before: {self.desired_angle_list}')
             self.desired_angle_list[:4] = self.ikpy_to_degree_conversion(ikpy_angle_list)
