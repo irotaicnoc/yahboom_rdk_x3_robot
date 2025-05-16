a8c58c575059ca0a24bbb5ae3aec66f79c1cb9e1
Marco <irotaicnoc@gmail.com>
2025-05-16 13:46:02 +0200

    fix: use function to automatically set correct run_time

--- sunriseRobot/app_SunriseRobot/physical_accessories/arm.py
+++ sunriseRobot/app_SunriseRobot/physical_accessories/arm.py
@@ -181,9 +181,9 @@ class Arm:
     def sub_mode_ik_start_callback(self) -> None:
         # this function is called when the arm is switched to inverse kinematics sub mode
 
-        # self.desired_angle_list = self.get_safe_arm_angle_list(clamped=True, default_value=90)
+        # self.set_desired_angles(self.get_safe_arm_angle_list(clamped=True, default_value=90))
         # convenient starting position for the gripper
-        self.desired_angle_list = [90, 45, 35, 35, 90, 90]
+        self.set_desired_angles([90, 45, 35, 35, 90, 90])
         print(f'desired_angle_list: {self.desired_angle_list}')
 
         self.gripper_speed = [0, 0, 0]
@@ -195,4 +195,4 @@ class Arm:
     def sub_mode_fk_start_callback(self) -> None:
         # this function is called when the arm is switched to forward kinematics sub mode
         self.servo_speed_list = [0, 0, 0, 0, 0, 0]
-        self.desired_angle_list = self.get_safe_arm_angle_list(clamped=True, default_value=90)
+        self.set_desired_angles(self.get_safe_arm_angle_list(clamped=True, default_value=90))
