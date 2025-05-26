4d979d81c2100453005dffe224a96c37a7c8a198
Marco <irotaicnoc@gmail.com>
2025-05-26 19:17:21 +0200

    fix test launcher and inverse_kinematic test

--- sunriseRobot/app_SunriseRobot/test_code/inverse_kinematic.py
+++ sunriseRobot/app_SunriseRobot/test_code/inverse_kinematic.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from ikpy.chain import Chain
-import ikpy.urdf.utils as urdf_utils
+# import ikpy.urdf.utils as urdf_utils
 from ikpy.inverse_kinematics import inverse_kinematic_optimization
 
 
@@ -11,58 +11,72 @@ def ikpy_to_degree_conversion(angles: np.array) -> np.array:
     return converted_angles
 
 
-my_chain = Chain.from_urdf_file(
-    urdf_file='urdf/arm.urdf',
-    base_elements=['base_link'],
-    name='arm',
-    active_links_mask=[False, True, True, True, True, False],
-)
-# my_chain.to_json_file(force=True)
-# x y z coordinates of the end effector
-target_position = np.array([0.1, 0.03, 0.1])
-start_time = time.time()
-joint_pos = my_chain.inverse_kinematics(target_position=target_position)
-print(f'chain IK time: {round(time.time() - start_time, 3)} seconds')
-print(f'joint_pos: {ikpy_to_degree_conversion(joint_pos)}')
-# for target_position [0.1, 0.15, 0.1]
-joint_initial_pos = np.array([0, -0.58916036, -0.62150267, -1.20038217, -0.56441146, 0])
-target_frame = np.zeros(shape=(3, 3))
-target_frame[:3, -1] = target_position
-start_time = time.time()
-joint_pos_2 = inverse_kinematic_optimization(
-    chain=my_chain,
-    target_frame=target_frame,
-    starting_nodes_angles=joint_initial_pos,
-    max_iter=20,
-    # regularization_parameter=None,
-)
-print(f'standalone IK time: {round(time.time() - start_time, 3)} seconds')
-print(f'joint_pos: {ikpy_to_degree_conversion(joint_pos_2)}')
-# pos 0: ???
-# pos 1: servo 1 (rotate base)
-# pos 2: servo 2 (tilt whole arm)
-transformation_matrix = my_chain.forward_kinematics(joints=joint_pos)
-print(f'position :\n{transformation_matrix[:3, 3]}')
-transformation_matrix_2 = my_chain.forward_kinematics(joints=joint_pos_2)
-print(f'position_2 :\n{transformation_matrix_2[:3, 3]}')
-
-# plot the arm in 3D in two subplots
-fig = plt.figure(figsize=(12, 6))
-ax1 = fig.add_subplot(121, projection='3d')
-ax2 = fig.add_subplot(122, projection='3d')
-
-my_chain.plot(joint_pos, ax1)
-ax1.set_title('my_chain')
-my_chain.plot(joint_pos_2, ax2)
-ax2.set_title('my_chain_2')
-
-plt.show()
+def degree_to_ikpy_conversion(angles: np.array) -> np.array:
+    converted_angles = np.deg2rad(angles - 90)
+    return converted_angles
 
 
-# Get the URDF tree
-# dot, urdf_tree = urdf_utils.get_urdf_tree(
-#     urdf_path='arm.urdf',
-#     out_image_path='C:/Users/Marco/GIT/yahboom_rdk_x3_robot/sunriseRobot/app_SunriseRobot/img',
-#     root_element='base_link',
-#     legend=True,
-# )
+def main():
+    my_chain = Chain.from_urdf_file(
+        urdf_file='urdf/arm.urdf',
+        base_elements=['base_link'],
+        name='arm',
+        active_links_mask=[False, True, True, True, True, False],
+    )
+    # my_chain.to_json_file(force=True)
+    # x y z coordinates of the end effector
+    # target_position = np.array([0.1, 0.03, 0.1])
+    # start_time = time.time()
+    # joint_pos = my_chain.inverse_kinematics(target_position=target_position)
+    # print(f'chain IK time: {round(time.time() - start_time, 3)} seconds')
+    # print(f'joint_pos: {ikpy_to_degree_conversion(joint_pos)}')
+    # for target_position [0.1, 0.15, 0.1]
+    joint_initial_pos = [90, 45, 35, 35, 90, 90]
+    print(f'joint_initial_pos: {joint_initial_pos}')
+    joint_initial_pos = degree_to_ikpy_conversion(joint_initial_pos)
+    print(f'joint_initial_pos: {joint_initial_pos}')
+    transformation_matrix = my_chain.forward_kinematics(joints=joint_initial_pos)
+    gripper_position = transformation_matrix[:3, 3]
+    print(f'gripper position :\n{gripper_position}')
+    target_frame = np.zeros(shape=(3, 3))
+    target_frame[:3, -1] = gripper_position
+    start_time = time.time()
+    joint_pos_2 = inverse_kinematic_optimization(
+        chain=my_chain,
+        target_frame=target_frame,
+        starting_nodes_angles=joint_initial_pos,
+    )
+    print(f'standalone IK time: {round(time.time() - start_time, 3)} seconds')
+    print(f'joint_pos: {ikpy_to_degree_conversion(joint_pos_2)}')
+    # pos 0: ???
+    # pos 1: servo 1 (rotate base)
+    # pos 2: servo 2 (tilt whole arm)
+    # transformation_matrix = my_chain.forward_kinematics(joints=joint_pos)
+    # gripper_position = transformation_matrix[:3, 3]
+    # print(f'gripper position :\n{gripper_position}')
+    # transformation_matrix_2 = my_chain.forward_kinematics(joints=joint_pos_2)
+    # print(f'gripper position_2 :\n{transformation_matrix_2[:3, 3]}')
+
+    # plot the arm in 3D in two subplots
+    fig = plt.figure(figsize=(12, 6))
+    ax1 = fig.add_subplot(121, projection='3d')
+    ax2 = fig.add_subplot(122, projection='3d')
+
+    my_chain.plot(joint_initial_pos, ax1)
+    ax1.set_title('my_chain')
+    my_chain.plot(joint_pos_2, ax2)
+    ax2.set_title('my_chain_2')
+
+    plt.show()
+
+    # Get the URDF tree
+    # dot, urdf_tree = urdf_utils.get_urdf_tree(
+    #     urdf_path='arm.urdf',
+    #     out_image_path='C:/Users/Marco/GIT/yahboom_rdk_x3_robot/sunriseRobot/app_SunriseRobot/img',
+    #     root_element='base_link',
+    #     legend=True
+    # )
+
+
+if __name__ == '__main__':
+    main()
--- sunriseRobot/app_SunriseRobot/test_launcher.py
+++ sunriseRobot/app_SunriseRobot/test_launcher.py
@@ -1,18 +1,16 @@
-import args
+# import args
 # import utils
-import global_constants as gc
-from robot_body import RobotBody
-from test_code import buzzer_song_ff
-from test_code import buzzer_song_mario
+# import global_constants as gc
+# from robot_body import RobotBody
+from test_code import inverse_kinematic
 
 
 def main(**kwargs):
-    parameters = args.import_args(yaml_path=gc.CONFIG_FOLDER_PATH + 'main_thread.yaml', **kwargs)
-    robot_body = RobotBody(com=parameters['com'], baud_rate=parameters['baud_rate'], verbose=parameters['verbose'])
-    robot_body.create_receive_threading()
+    # parameters = args.import_args(yaml_path=gc.CONFIG_FOLDER_PATH + 'main_thread.yaml', **kwargs)
+    # robot_body = RobotBody(com=parameters['com'], baud_rate=parameters['baud_rate'], verbose=parameters['verbose'])
+    # robot_body.create_receive_threading()
 
-    buzzer_song_ff.play(robot_body=robot_body)
-    # buzzer_song_mario.play(robot_body=robot_body)
+    inverse_kinematic.main()
 
 
 if __name__ == '__main__':
