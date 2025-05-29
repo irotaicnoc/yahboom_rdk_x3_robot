788a7d09faed1a6d010135ca96c2244d154750f6
Marco <irotaicnoc@gmail.com>
2025-05-29 02:07:24 +0200

    added class for 2-pin button

---
+++ sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
@@ -0,0 +1,24 @@
+import Hobot.GPIO as GPIO
+
+import global_constants as gc
+
+
+class Button2Pin:
+    def __init__(self, control_cable: int, mode: str = GPIO.BOARD):
+        # Set the pin numbering mode to BOARD (1-40)
+        GPIO.setmode(mode)
+        self.control_cable = control_cable
+        self.pressed = False
+        GPIO.setup(self.control_cable, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def is_pressed(self):
+        """
+        Check if the button is pressed.
+        :return: True if pressed, False otherwise.
+        """
+        self.pressed = GPIO.input(self.control_cable) == GPIO.LOW
+        return self.pressed
+
+    def __del__(self):
+        GPIO.cleanup()
+        
