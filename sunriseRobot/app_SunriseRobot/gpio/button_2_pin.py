cbd7de5d4cffd8f36136122f2a8f8b012c952f18
Marco <irotaicnoc@gmail.com>
2025-05-30 23:24:45 +0200

    test button

--- sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
+++ sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
@@ -11,12 +11,19 @@ class Button2Pin:
         GPIO.setup(self.control_cable, GPIO.IN)
 
         GPIO.add_event_detect(self.control_cable, GPIO.RISING, callback=callback, bouncetime=200)
-        GPIO.add_event_callback(self.control_cable, self.info)
+        GPIO.add_event_callback(self.control_cable, self.rising_detected)
+
+        GPIO.add_event_detect(self.control_cable, GPIO.FALLING, callback=callback, bouncetime=200)
+        GPIO.add_event_callback(self.control_cable, self.falling_detected)
         # print()
 
     @staticmethod
-    def info(self):
-        print('button pressed')
+    def rising_detected():
+        print('rising detected')
+
+    @staticmethod
+    def falling_detected():
+        print('falling detected')
 
     def __del__(self):
         GPIO.cleanup(self.control_cable)
