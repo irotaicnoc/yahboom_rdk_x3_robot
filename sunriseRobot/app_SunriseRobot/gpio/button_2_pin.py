08a52a8c20f6c43b75a93c1d3145a22b13af3f7e
Marco <irotaicnoc@gmail.com>
2025-05-30 23:46:43 +0200

    test button

--- sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
+++ sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
@@ -10,12 +10,23 @@ class Button2Pin:
         self.control_cable = control_cable
         GPIO.setup(self.control_cable, GPIO.IN)
 
-        GPIO.add_event_detect(self.control_cable, GPIO.RISING, callback=rising_detected)
-        # GPIO.add_event_callback(self.control_cable, self.rising_detected)
+        # GPIO.add_event_detect(self.control_cable, GPIO.RISING, callback=rising_detected)
+        # # GPIO.add_event_callback(self.control_cable, self.rising_detected)
+        #
+        # GPIO.add_event_detect(self.control_cable, GPIO.FALLING, callback=falling_detected)
+        # # GPIO.add_event_callback(self.control_cable, self.falling_detected)
+        # # print()
 
-        GPIO.add_event_detect(self.control_cable, GPIO.FALLING, callback=falling_detected)
-        # GPIO.add_event_callback(self.control_cable, self.falling_detected)
-        # print()
+    def press_listener(self, callback: callable):
+        """
+        Listen for button presses and call the provided callback function.
+        :param callback: Function to call when the button is pressed.
+        """
+        print('started listening for button presses')
+        GPIO.wait_for_edge(self.control_cable, GPIO.RISING)
+        print('rising detected')
+        callback()
+        print('callback executed after rising edge detected')
 
     def __del__(self):
         GPIO.cleanup(self.control_cable)
--- sunriseRobot/app_SunriseRobot/main_thread.py
+++ sunriseRobot/app_SunriseRobot/main_thread.py
@@ -29,15 +29,11 @@ def main_loop(**kwargs):
     led_2_pin = Led2Pin(power_cable=gc.GREEN_CABLE_02)
     button_2_pin = Button2Pin(control_cable=gc.BROWN_CABLE_01, callback=led_2_pin.toggle_state)
     print('finished initializing GPIO pins')
-    led_2_pin.set_state(on=True)
-    time.sleep(3)
-    led_2_pin.set_state(on=False)
-    print('led tested')
-    # thread_button_2_pin = threading.Thread(
-    #     target=button_2_pin.pressed_callback,
-    #     name='task_button_listener',
-    #     kwargs={'callback': led_2_pin.toggle_state},
-    # )
+    thread_button_2_pin = threading.Thread(
+        target=button_2_pin.press_listener,
+        name='task_button_press_listener',
+        kwargs={'callback': led_2_pin.toggle_state},
+    )
 
     robot_head = RobotHead(
         robot_body=robot_body,
