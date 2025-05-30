23309fe4e6496082fe7a696b987d4f3d44a97e44
Marco <irotaicnoc@gmail.com>
2025-05-30 23:03:52 +0200

    test different button logic

--- sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
+++ sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
@@ -4,15 +4,17 @@ import global_constants as gc
 
 
 class Button2Pin:
-    def __init__(self, control_cable: int, mode: str = GPIO.BOARD):
+    def __init__(self, control_cable: int, callback: callable, mode: str = GPIO.BOARD):
         # Set the pin numbering mode to BOARD (1-40)
         GPIO.setmode(mode)
         self.control_cable = control_cable
         self.pressed = False
         GPIO.setup(self.control_cable, GPIO.IN)
 
-    def pressed_callback(self, callback: callable):
+    # def pressed_callback(self, callback: callable):
+        print('Button2Pin: waiting for button press...')
         GPIO.add_event_detect(self.control_cable, GPIO.RAISING, callback=callback, bouncetime=200)
+        # print()
 
     def __del__(self):
         GPIO.cleanup(self.control_cable)
--- sunriseRobot/app_SunriseRobot/main_thread.py
+++ sunriseRobot/app_SunriseRobot/main_thread.py
@@ -27,12 +27,12 @@ def main_loop(**kwargs):
     internal_light = Light(verbose=parameters['verbose'])
     led_3_pin = Led3Pin(red_power_cable=gc.VIOLET_CABLE_01, green_power_cable=gc.GREEN_CABLE_01)
     led_2_pin = Led2Pin(power_cable=gc.GREEN_CABLE_02)
-    button_2_pin = Button2Pin(control_cable=gc.BROWN_CABLE_01)
-    thread_button_2_pin = threading.Thread(
-        target=button_2_pin.pressed_callback,
-        name='task_button_listener',
-        kwargs={'callback': led_2_pin.toggle_state},
-    )
+    button_2_pin = Button2Pin(control_cable=gc.BROWN_CABLE_01, callback=led_2_pin.toggle_state)
+    # thread_button_2_pin = threading.Thread(
+    #     target=button_2_pin.pressed_callback,
+    #     name='task_button_listener',
+    #     kwargs={'callback': led_2_pin.toggle_state},
+    # )
 
     robot_head = RobotHead(
         robot_body=robot_body,
