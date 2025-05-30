c65b669871184fa3c6f655c09d98d83c49845fff
Marco <irotaicnoc@gmail.com>
2025-05-30 23:21:05 +0200

    test button

--- sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
+++ sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
@@ -8,13 +8,15 @@ class Button2Pin:
         # Set the pin numbering mode to BOARD (1-40)
         GPIO.setmode(mode)
         self.control_cable = control_cable
-        self.pressed = False
         GPIO.setup(self.control_cable, GPIO.IN)
 
-    # def pressed_callback(self, callback: callable):
-        print('Button2Pin: waiting for button press...')
         GPIO.add_event_detect(self.control_cable, GPIO.RISING, callback=callback, bouncetime=200)
+        GPIO.add_event_callback(self.control_cable, self.info)
         # print()
+
+    @staticmethod
+    def info(self):
+        print('button pressed')
 
     def __del__(self):
         GPIO.cleanup(self.control_cable)
