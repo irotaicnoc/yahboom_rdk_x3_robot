6532656e0dde6a97d4858a0cb8013c764ffceaec
Marco <irotaicnoc@gmail.com>
2025-05-31 16:22:11 +0200

    test button

--- sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
+++ sunriseRobot/app_SunriseRobot/gpio/button_2_pin.py
@@ -7,12 +7,14 @@ class Button2Pin:
     def __init__(self, control_cable: int, mode: str = GPIO.BOARD):
         # Set the pin numbering mode to BOARD (1-40)
         # GPIO.setmode(mode)
+        print(f'GPIO VERSION: {GPIO.VERSION}')
+        print(f'GPIO model: {GPIO.model}')
         self.control_cable = control_cable
         try:
             GPIO.cleanup(self.control_cable)
         except Exception:
             pass
-        GPIO.setup(self.control_cable, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.control_cable, GPIO.IN, pull_up_down='Pull-up')
 
         # GPIO.add_event_detect(self.control_cable, GPIO.RISING, callback=rising_detected)
         # # GPIO.add_event_callback(self.control_cable, self.rising_detected)
