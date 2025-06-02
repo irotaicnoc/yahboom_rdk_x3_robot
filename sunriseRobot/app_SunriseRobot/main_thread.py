1fd304bb9078e9172cb38bcfda671f7642774139
Marco <irotaicnoc@gmail.com>
2025-06-02 16:02:50 +0200

    fix: red-black button callback assignment

--- sunriseRobot/app_SunriseRobot/main_thread.py
+++ sunriseRobot/app_SunriseRobot/main_thread.py
@@ -64,8 +64,8 @@ def main_loop(**kwargs):
     #    with long click: toggle GUI mode
     button_2_pin_rb_listener_kwargs = {
             'control_cable': gc.RED_CABLE_02,
-            'callback_short_click': robot_head.next_mode(),
-            # 'callback_long_click': robot_head.toggle_gui_mode(),
+            'callback_short_click': robot_head.next_mode,
+            'callback_long_click': robot_head.toggle_gui_mode,
             'button_press_required_time': robot_head.button_press_required_time,
     }
     thread_button_2_pin_rb = threading.Thread(
