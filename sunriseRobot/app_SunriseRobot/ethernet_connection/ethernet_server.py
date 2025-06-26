61a9a98e06767c9123b6c488ec61b9d0e66c836a
Marco <irotaicnoc@gmail.com>
2025-06-26 17:45:52 +0200

    fix: removed wrong debug print

--- sunriseRobot/app_SunriseRobot/ethernet_connection/ethernet_server.py
+++ sunriseRobot/app_SunriseRobot/ethernet_connection/ethernet_server.py
@@ -88,7 +88,6 @@ class ConnectionHandler:
             decoded_data = self.receive_function_call()
             if decoded_data is not None:
                 try:
-                    print('Executing function ' + decoded_data['name'] + ' with parameters ' + decoded_data['args'])
                     self.function_caller.call_function(function_name=decoded_data['name'], kwargs=decoded_data['args'])
                 except Exception as e:
                     utils.print_exception(exception=e, message='Error when executing received function')
