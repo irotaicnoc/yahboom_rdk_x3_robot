e951bf8205183c5e7ff507e02d734ce81e4e867e
Marco <irotaicnoc@gmail.com>
2025-06-28 15:38:51 +0200

    changed available function structure

--- sunriseRobot/app_SunriseRobot/function_calls/available_functions.py
+++ sunriseRobot/app_SunriseRobot/function_calls/available_functions.py
@@ -1,33 +1,45 @@
-function_list = [
-    {
-        'name': 'set_beep',
-        'containing_object': 'robot_body',
-    },
-    {
-        'name': 'set_car_motion',
-        'containing_object': 'robot_body',
-    },
-    {
-        'name': 'toggle_rigid',
-        'containing_object': 'arm',
-    },
-    {
-        'name': 'set_desired_angles',
-        'containing_object': 'arm',
-    },
-    {
-        'name': 'next_light_effect',
-        'containing_object': 'light',
-    },
-    {
-        'name': 'stop',
-        'containing_object': 'light',
-    },
-]
 
-object_list = [
-    {
-        'name': 'robot_head',
-        'excluded_functions': ['add_mode_callback', 'add_sub_mode_callback', 'all_sub_modes'],
-    },
-]
+
+class AvailableFunctions:
+    """
+    This class contains the robot functions available for function calls with voice interaction.
+    """
+
+    def __init__(self, robot_body, robot_head, arm=None, light=None):
+        """
+        Initializes the AvailableFunctions class with the modules from which the functions come.
+        """
+        self.robot_body = robot_body
+        if arm is not None:
+            self.arm = arm
+        if light is not None:
+            self.light = light
+
+        for attr in dir(robot_head):
+            if not attr.startswith('_') and callable(getattr(robot_head, attr)):
+                if attr not in ['add_mode_callback', 'add_sub_mode_callback', 'all_sub_modes']:
+                    locals()[attr] = getattr(robot_head, attr)
+
+    def beep(self, seconds: float):
+        seconds = round(seconds, 2)
+        self.robot_body.set_beep(on_time=seconds * 1000)
+
+    def set_arm_state(self, rigid: bool):
+        if self.arm is None:
+            raise ValueError("Arm module is not initialized.")
+        self.arm.toggle_rigid(rigid=rigid)
+
+    def set_arm_angles(self, angles: list):
+        if self.arm is None:
+            raise ValueError("Arm module is not initialized.")
+        self.arm.set_desired_angles(angles=angles)
+
+    def change_light_effect(self):
+        if self.light is None:
+            raise ValueError("Internal light module is not initialized.")
+        self.light.next_light_effect()
+
+    def turn_off_lights(self):
+        if self.light is None:
+            raise ValueError("Internal light module is not initialized.")
+        self.light.stop()
--- sunriseRobot/app_SunriseRobot/function_calls/function_caller.py
+++ sunriseRobot/app_SunriseRobot/function_calls/function_caller.py
@@ -1,7 +1,6 @@
-import time
 import warnings
 
-from function_calls import available_functions
+from function_calls.available_functions import AvailableFunctions
 
 
 class FunctionCaller:
@@ -10,54 +9,24 @@ class FunctionCaller:
     """
 
     def __init__(self, robot_head, robot_body, arm=None, light=None, verbose: int = 0):
-        self.function_list = available_functions.function_list
-        self.available_objects = available_functions.object_list
-        self.robot_head = robot_head
-        self.robot_body = robot_body
-        self.arm = arm
-        self.light = light
+        self.available_functions = AvailableFunctions(
+            robot_head=robot_head,
+            robot_body=robot_body,
+            arm=arm,
+            light=light,
+        )
         self.verbose = verbose
-
-    def find_function(self, function_name: str):
-        """
-        Finds the function in the available functions list and as attributes of object_list.
-        :param function_name: The name of the function to find.
-        :return: The function details if found, otherwise None.
-        """
-        print(f'Finding function: {function_name}')
-        for function in self.function_list:
-            if function['name'] == function_name:
-                return function
-        for obj in self.available_objects:
-            if function_name in obj['excluded_functions']:
-                continue
-            if hasattr(obj, function_name):
-                return {
-                    'name': function_name,
-                    'containing_object': obj['name'],
-                }
-        return None
+        for attr in dir(self.available_functions):
+            if callable(getattr(self.available_functions, attr)):
+                print(f'available_functions contains callable attr:\n\t"{attr}".')
 
     def call_function(self, function_name: str, kwargs) -> None:
         """
         Calls the function with the given arguments and keyword arguments.
         """
-        function = self.find_function(function_name=function_name)
-        if function is None:
-            warnings.warn(f'Function "{function_name}" not found in available functions.')
-            return
-
-        containing_object = function['containing_object']
-
-        # Call the function on the containing object
-        obj = getattr(self, containing_object, None)
-        if obj is None:
-            warnings.warn(f'Object "{containing_object}" for function calling not found.')
-        print(f'Calling function "{function_name}" on object "{containing_object}".')
-
-        method = getattr(obj, function_name, None)
+        method = getattr(self.available_functions, function_name, None)
         if method is None:
-            warnings.warn(f'Method "{function_name}" not found in object "{containing_object}".')
+            warnings.warn(f'Method "{function_name}" not found in available functions.')
         print(f'Calling method: {method.__name__}')
 
         if kwargs is None:
@@ -66,4 +35,3 @@ class FunctionCaller:
         else:
             print(f'\twith arguments {kwargs}.')
             method(**kwargs)
-        time.sleep(1)
