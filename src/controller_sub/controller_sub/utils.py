165746c2b6fd71fbbb2ac9366ae9cd346706c100
Marco <marco.conciatori@unipr.it>
2026-03-29 17:17:17 +0200

    refactoring: removed unused ros2 nodes

--- src/controller_sub/controller_sub/utils.py
+++
@@ -1,8 +0,0 @@
-# standard libraries
-import yaml
-
-
-def args_from_yaml(config_path: str) -> dict:
-    with open(config_path) as config_file:
-        arg_dict = yaml.safe_load(config_file)
-    return arg_dict
