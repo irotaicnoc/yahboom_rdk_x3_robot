3d0c1f2e790c317f405ca25ab03b1379eb86c311
Marco <marco.conciatori@unipr.it>
2026-04-01 23:34:50 +0200

    fix: removed logs

--- src/camera_pub/camera_pub/camera_publisher.py
+++ src/camera_pub/camera_pub/camera_publisher.py
@@ -75,8 +75,8 @@ class CameraPublisherNode(Node):
 
         # ros2_image_message = self.cv_ros_bridge.cv2_to_imgmsg(frame, encoding='rgb8')
         ros2_image_message = utils.jpeg_to_compressed_img_msg(frame)
-        self.get_logger().info(f'image {self.message_counter}, shape {frame.shape}')
-        self.get_logger().info(f'encoding {ros2_image_message.encoding}')
+        # self.get_logger().info(f'image {self.message_counter}, shape {frame.shape}')
+        # self.get_logger().info(f'encoding {ros2_image_message.encoding}')
         self.publisher.publish(ros2_image_message)
 
         # if self.message_counter % 100 == 0:
