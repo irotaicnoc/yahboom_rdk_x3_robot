fa7c45759b2db256ded2639bce6912369f6d979d
Marco <marco.conciatori@unipr.it>
2026-05-01 21:53:32 +0200

    removed reference code

--- yahboomcar_ws_src/src/yahboomcar_astra/opencv_examples/2_1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-if __name__ == '__main__':
-	img = cv2.imread('figures/yahboom.jpg')
-	print(img.shape)
-	x, y = img.shape[0:2]
-	img_resize = cv2.resize(img, (int(y / 2),   int(x / 2)))
-	cv2.imwrite('figures/2_1.jpg', img_resize)
-	print(img_resize.shape)
--- yahboomcar_ws_src/src/yahboomcar_astra/opencv_examples/2_2.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-if __name__ == '__main__':
-	img = cv2.imread('figures/yahboom.jpg')
-	dst = img[0:100,100:200]
-	cv2.imwrite('figures/2_2.jpg', dst)
